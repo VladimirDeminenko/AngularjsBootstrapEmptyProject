7aa4523cedb11a8c63dfa5f53ab9d87394cfa47a
Vladimir Deminenko <vladimir.deminenko+github@gmail.com>
2013-06-30 11:37:40 +0200

    step-0

---
+++ app/js/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+    .controller('MyCtrl2', [function() {
+
+    }])
+    .controller('MyCtrl3', [function() {
+}]);
